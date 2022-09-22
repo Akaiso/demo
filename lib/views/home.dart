7b04d6796048f27f29334b85914ae01016df5e93
Akaiso <akaisobenson@yahoo.com>
2022-09-22 19:03:38 +0100

    first commit with dependencies

---
+++ lib/views/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('DEMO'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(),
+          ),
+        ),
+    );
+  }
+}
